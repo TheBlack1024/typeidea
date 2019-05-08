22f9b40874e55819e73bb3d68d5d9b98fc56700b
谭鹤鸣 <92182005@qq.com>
2019-05-08 15:04:21 +0800

    Django 7.3

--- typeidea/typeidea/blog/views.py
+++ typeidea/typeidea/blog/views.py
@@ -15,10 +15,14 @@ def post_list(request,category_id=None,tag_id=None):
 def post_detail(request,post_id):
     return HttpResponse('detail')
 """
-
-from .models import Post,Tag
+from .models import Post,Tag,Category
+from config.models import SideBar
 
 def post_list(request,category_id=None,tag_id=None):
+    tag = None
+    category = None
+
+    """
     if tag_id:
         try:
             tag = Tag.objects.get(id=tag_id)
@@ -29,9 +33,28 @@ def post_list(request,category_id=None,tag_id=None):
     else:
         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
         if category_id:
-            post_list = post_list.filter(category_id=category_id)
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                category = None
+            else:
+                post_list = post_list.filter(category_id=category_id)
+    """
+    if tag_id:
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
+    else:
+        post_list = Post.latest_posts()
 
-    return render(request,'blog/list.html',context={'post_list':post_list})
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+        'sidebars': SideBar.get_all(),
+    }
+    context.update(Category.get_navs())
+    return render(request,'blog/list.html',context=context)
 
 
 def post_detail(request,post_id=None):
@@ -40,5 +63,10 @@ def post_detail(request,post_id=None):
     except Post.DoesNotExist:
         post = None
 
-    return render(request,'blog/detail.html',context={'post':post})
+    context = {
+        'post': post,
+        'sidebars': SideBar.get_all(),
+    }
+    context.update(Category.get_navs())
+    return render(request,'blog/detail.html',context=context)
 
--- typeidea/typeidea/comment/models.py
+++ typeidea/typeidea/comment/models.py
@@ -22,4 +22,4 @@ class Comment(models.Model):
         verbose_name = verbose_name_plural = "评论"
 
     def __str__(self):
-        return self.name
+        return self.nickname
